fbf6bd28a822861abae66abac23647842d2c38d4
Mark <wangzhuofeng@gmail.com>
2017-07-15 15:48:42 +0800

    add technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |tech|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{tech}"
+  )
+end
+
+puts "3 technologies created."
